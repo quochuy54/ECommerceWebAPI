2589111bb1a80ccbae4ab149e22847f4bcbd890b
quochuy001 <quochuyqh01@gmail.com>
2022-08-09 17:36:28 +0700

    Ecommerce-API-v1.1-add-Procfile

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 const route = require('./route/index');
 require('dotenv/config');
+const cors=require("cors");
 
 //Middleware
 app.use(express.json());
 app.use('/public/uploads', express.static(__dirname + "\\public\\uploads"));
+//COR
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 // Connect MongoDB
 mongoose.connect(process.env.DB_URL, {
